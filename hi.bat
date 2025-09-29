22119b398a17be7d51da024073d877fc56de9c0b
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2025-09-29 15:48:25 +0530

    created a springboot project

---
+++ hi.bat
@@ -0,0 +1,3 @@
+git remote add origin https://github.com/srivishnuvarthan-07/Ecommerce.git
+git branch -M main
+git push -u origin main
---
+++ src/main/java/com/ecommerce/service/ecommerceapp.java
@@ -0,0 +1,11 @@
+package com.ecommerce.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ecommerceapp {
+    public static void main(String[] args) {
+        SpringApplication.run(ecommerceapp.class, args);
+    }
+}
