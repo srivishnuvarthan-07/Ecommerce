af6506738f2448f2ce3851f8da8b9bfe0ddc26d9
srivishnuvarthan-07 <kit27.ad54@gmail.com>
2025-09-29 15:51:45 +0530

    batfilecreated

--- hi.bat
+++ hi.bat
@@ -1,3 +1,4 @@
-git remote add origin https://github.com/srivishnuvarthan-07/Ecommerce.git
-git branch -M main
-git push -u origin main
+git add .
+git commit -m "batfilecreated"
+git push 
+notepad oooo.cpp
